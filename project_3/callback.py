4f6c73a8557ef5f63ec92ca5623259bd42c13809
shwan-goofy <lshn1007@hanyang.ac.kr>
2023-12-14 19:41:45 +0900

    feat: template 기반 정보 조회가 가능하게 기능 구현

--- project_3/callback.py
+++ project_3/callback.py
@@ -1,82 +1,115 @@
-import aiohttp
 import os
-import time
-import logging
 
-import openai
+import requests
+from langchain.chains import ConversationChain, LLMChain
 from langchain.chat_models import ChatOpenAI
-from langchain.agents import initialize_agent, AgentType, Tool
-from langchain.chains import LLMMathChain
-
-from dto import ChatbotRequest
-from db import get_kakao_sync_data, get_kakao_channel_data, get_kakao_social_data
+from langchain.prompts.chat import ChatPromptTemplate
 from dotenv import load_dotenv
 
-load_dotenv()
-openai.api_key = os.environ["OPENAI_API_KEY"]
-SYSTEM_MSG = """
-You are a Kakao service provider. Please kindly answer user questions.
-The conditions below must be met.
-- Please answer in Korean.
-- Please answer structurally.
-- Please summarize your answer in about 200 characters.
-"""
-llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k")
-llm_match_chain = LLMMathChain.from_llm(llm=llm, verbose=True)
-tools = [
-    Tool(
-        name="get_kakao_sync_data",
-        func=get_kakao_sync_data,
-        description="Used for searches related to Kakao Sync(카카오 싱크).",
-    ),
-    Tool(
-        name="get_kakao_channel_data",
-        func=get_kakao_channel_data,
-        description="Used for searches related to Kakao Channel(카카오 채널).",
-    ),
-    Tool(
-        name="get_kakao_social_data",
-        func=get_kakao_social_data,
-        description="Used for searches related to Kakao Social(카카오 소셜).",
-    ),
-]
+from db import query_db
+from dto import ChatbotRequest
 
-agent = initialize_agent(
-    tools,
-    llm,
-    agent=AgentType.OPENAI_FUNCTIONS,  # zero shot으로 하면 영어로 답하고 무한 루프 돎
+
+load_dotenv()
+assert os.environ["OPENAI_API_KEY"] is not None
+
+"""
+Series of Chains
+"""
+
+llm = ChatOpenAI(temperature=0, max_tokens=250, model="gpt-3.5-turbo")
+
+parse_intent_chain = LLMChain(
+    llm=llm,
+    prompt=ChatPromptTemplate.from_template(
+        template="""
+            Your job is to select one intent from the <intent_list>. Intent is a proper noun and is a word surrounded by square brackets.
+
+            <intent_list>
+            1. [kakao_sync]: This document introduces KakaoSync and guides you through the inspection and settings required to introduce KakaoSync. The Korean word of kakao sync is 카카오 싱크. 
+            2. [kakao_channel]: This document introduces the KakaoTalk Social API. KakaoTalk Social API provides the user's KakaoTalk profile and friend information to implement the service's social functions. You can also send KakaoTalk messages based on the provided KakaoTalk friend information. The Korean word of kakao social is 카카오 소셜.
+            3. [kakao_social]: This document introduces the KakaoTalk Channel API. The Korean word of kakao social is 카카오 소셜.
+            </intent_list>
+
+            User: {user_message}
+            Intent: 
+        """
+    ),
     verbose=True,
-    handle_parsing_errors=True,
-    max_iterations=4,  # 무한 루프 방지
+)
+answer_chain = LLMChain(
+    llm=llm,
+    prompt=ChatPromptTemplate.from_template(
+        template="""
+            Your job is to answer the query based on information.
+
+            <Information>
+            {information}
+            <Iinformation>
+
+            <Query>
+            {query}
+            </Query>
+        """
+    ),
+    verbose=True
+)
+
+summary_chain = LLMChain(
+    llm=llm,
+    prompt=ChatPromptTemplate.from_template(
+        template="""
+            Your role is to summarize the answer in 3 sentences or so.
+            <answer>
+             {expert_answer}
+            </answer>
+        """
+    ),
+    verbose=True
 )
 
 
-async def callback_handler(request: ChatbotRequest) -> dict:
-    messages = [
-        {"role": "system", "content": SYSTEM_MSG},
-        {"role": "user", "content": request.userRequest.utterance},
-    ]
-
-    output_text = agent.run(messages)
-    await response_callback(output_text, request)
 
 
-async def response_callback(output_text, request):
-	payload = {
+default_chain = ConversationChain(llm=llm)
+
+"""
+business logic
+"""
+
+
+def callback_handler(request: ChatbotRequest):
+    msg = request.userRequest.utterance
+
+    context = {}
+    intent = parse_intent_chain.run(msg)
+    if intent in ["kakao_sync", "kakao_channel", "kakao_social"]:
+        infos = query_db(msg)
+        information_str = "\n".join(infos)
+
+        context['information'] = information_str
+        context['query'] = msg
+
+        output = answer_chain.run(context)
+    else:
+        output = default_chain.run(msg)
+
+    context["expert_answer"] = output
+    answer = summary_chain.run(context)
+
+    response_callback(answer, request)
+
+
+def response_callback(output_text, request):
+    payload = {
         "version": "2.0",
         "template": {
             "outputs": [
-                {
-                    "simpleText": {
-                        "text": output_text
-                    }
-                }
+                {"simpleText": {"text": output_text}}
             ]
         }
     }
-	url = request.userRequest.callbackUrl
+    url = request.userRequest.callbackUrl
 
-	if url:
-		async with aiohttp.ClientSession() as session:
-			async with session.post(url=url, json=payload, ssl=False) as resp:
-				await resp.json()
+    if url:
+        requests.post(url=url, json=payload)
--- project_3/db.py
+++ project_3/db.py
@@ -1,19 +1,46 @@
-def get_kakao_sync_data(*args):
-    print(args)
-    path = "data/kakao_sync.txt"
-    with open(path, "r") as fin:
-        return fin.read()
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import TextLoader
+from langchain.vectorstores import Chroma
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+CHROMA_COLLECTION_NAME = "kakao-bot"
+CHROMA_PERSIST_DIR = "./chromadb"
+
+"""
+실습 때 사용한 코드 재사용
+"""
+
+def upload_embedding_from_file(file_path):
+    documents = TextLoader(file_path).load()
+
+    text_splitter = CharacterTextSplitter(chunk_size=700, chunk_overlap=150)
+    docs = text_splitter.split_documents(documents)
+    ids = [f"{file_path}-{i}" for i in range(len(docs))]
+
+    Chroma.from_documents(
+        docs,
+        OpenAIEmbeddings(),
+        collection_name=CHROMA_COLLECTION_NAME,
+        persist_directory=CHROMA_PERSIST_DIR,
+        ids=ids,
+    )
+    print('db success')
 
 
-def get_kakao_channel_data(*args):
-    print(args)
-    path = "data/kakao_channel.txt"
-    with open(path, "r") as fin:
-        return fin.read()
+def upload_files():
+    files = ["kakao_sync.txt", "kakao_channel.txt", "kakao_social.txt"]
+    for file in files:
+        upload_embedding_from_file(f"data/{file}")
 
 
-def get_kakao_social_data(*args):
-    print(args)
-    path = "data/kakao_social.txt"
-    with open(path, "r") as fin:
-        return fin.read()
+def query_db(query):
+    upload_files()
+
+    db = Chroma(
+        persist_directory=CHROMA_PERSIST_DIR,
+        embedding_function=OpenAIEmbeddings(),
+        collection_name=CHROMA_COLLECTION_NAME,
+    )
+    docs = db.similarity_search(query, k=5)
+    str_docs = [doc.page_content for doc in docs]
+    return str_docs
